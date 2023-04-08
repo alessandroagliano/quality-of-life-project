8e9f8338b61aef581d3293627d79da114e29199b
AlessandroAgliano <calciosport@hotmail.it>
2023-04-08 23:15:12 +0200

    v 2.0

--- QualityOfLife-S2I/src/index.js
+++ QualityOfLife-S2I/src/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
+
+import "./dom-elements";
 import "bootstrap";
-import "./fetch";
 import "./progressbar.js";
 import "./progressbar.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
